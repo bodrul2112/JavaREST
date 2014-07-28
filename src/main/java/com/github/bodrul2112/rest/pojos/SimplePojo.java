5f1e34cb782750e848fd4f99ba9a1ac1ccd7ba47
Bodrul Alam <bodrul2112@gmail.com>
2014-07-28 17:19:43 +0100

    java rest api in 3 classes
    in 3 classes

---
+++ src/main/java/com/github/bodrul2112/rest/pojos/SimplePojo.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright 1995-2014 Caplin Systems Ltd
+ */
+package com.github.bodrul2112.rest.pojos;
+
+public class SimplePojo 
+{
+	private String nameOfTree = "tree1";
+	
+	private String nameOfBat = "bat2";
+	
+	public String getTree555()
+	{
+		return nameOfTree;
+	}
+	
+	public String getBat666()
+	{
+		return nameOfBat;
+	}
+}
